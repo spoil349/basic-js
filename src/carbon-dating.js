764adc4cdb7e4aa47255469a49c299459f7544aa
spoil349 <spoil349@mail.ru>
2021-03-27 23:17:10 +0400

    fix: Carbon dating decision

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,24 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleString) {
+
+  if (typeof(sampleString) != "string") {
+    return false;
+  }
+  number = +sampleString;
+  if (typeof(number) != 'number') {
+    return false
+  }
+    if (number <= 0) {
+      return false
+    }
+  if (number >= 15) {
+    return false
+  }
+  if (isNaN(number)) {
+  	return false
+  }
+  
+  return Math.ceil((Math.log(MODERN_ACTIVITY/number)/(0.693/HALF_LIFE_PERIOD)))  
 };
