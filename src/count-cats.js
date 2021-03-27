aa477d18f99d38cecf3db9937f3f19e0d6898578
spoil349 <spoil349@mail.ru>
2021-03-27 22:20:27 +0400

    fix: Cats count decision

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+    let counter = 0;
+    arr.forEach(element => {
+        
+        const kitten = '^^';
+        for (let i = 0; i < element.length; i++) {
+            if (element[i] == kitten) {
+                counter = counter + 1;
+            }
+        }
+    });
+    return counter;
 };
