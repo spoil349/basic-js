4fa2471ae7f2cd6d43d19fdf3975fe566e324b2c
spoil349 <spoil349@mail.ru>
2021-03-28 14:32:22 +0400

    fix: Dream team decision

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  let teamName = '';
+  let sortedSplittedMembers = [];
+  let upperCaseWord;
+
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) == 'string') {
+      upperCaseWord = members[i].toUpperCase();
+    	sortedSplittedMembers.push(upperCaseWord.split(' ').join(''));
+    }
+  }
+  
+  sortedSplittedMembers.sort();
+
+  for (let i = 0; i < sortedSplittedMembers.length; i++) {
+    if ((typeof(sortedSplittedMembers[i]) == 'string') &&
+    Boolean(sortedSplittedMembers[i]) &&
+    (sortedSplittedMembers[i].trim().length > 0)) {
+      teamName = teamName + sortedSplittedMembers[i][0];
+    }
+  }
+
+  return teamName.toUpperCase();
 };
